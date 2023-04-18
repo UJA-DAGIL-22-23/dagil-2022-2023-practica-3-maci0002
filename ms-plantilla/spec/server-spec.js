ae0929bfab1e25e3db502b38f87423981fd79f24
Miguel Ángel Carrasco Infante <maci0002@red.ujaen.es>
2023-04-18 17:12:38 +0200

    HU 1 completada con TDD y HU 2 comenzada

--- ms-plantilla/spec/server-spec.js
+++ ms-plantilla/spec/server-spec.js
@@ -26,6 +26,9 @@ describe('Servidor PLANTILLA:', () => {
           //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
           assert(res.body.hasOwnProperty('mensaje'));
           assert(res.body.mensaje === "Microservicio MS Plantilla: home");
+          assert(res.body.autor === "Miguel Angel Carrasco Infante");
+          assert(res.body.email === "maci0002@red.ujaen.es");
+          assert(res.body.fecha ===  "13/08/01");
 
         })
         .end((error) => { error ? done.fail(error) : done() })
@@ -49,15 +52,15 @@ describe('Servidor PLANTILLA:', () => {
    * Tests para acceso a la BBDD
    */
   describe('Acceso a BBDD:', () => {
-    it('Devuelve ¿¿¿ VALOR ESPERADO ??? al consultar mediante test_db', (done) => {
+    it('Devuelve Miguel al consultar mediante test_db', (done) => {
       supertest(app)
         .get('/test_db')
         .expect(200)
         .expect('Content-Type', /json/)
         .expect(function (res) {
           //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
-          assert(res.body.data[0].data.hasOwnProperty('¿¿¿ PROPIEDAD ???'));
-          assert(res.body.data[0].data.nombre === "¿¿¿ VALOR ESPERADO ???");
+          assert(res.body.data[0].data.hasOwnProperty('nombre'));
+          assert(res.body.data[0].data.nombre === "Miguel");
 
         })
         .end((error) => { error ? done.fail(error) : done(); }
